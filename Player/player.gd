5e6d13c31f09f28cfbb07169ffe861a714afeb5a
Pawanjot Randhawa <pawanjot2004@gmail.com>
2026-05-16 21:23:20 -0400

    yes

--- Player/player.gd
+++ Player/player.gd
@@ -6,20 +6,17 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	# Get Player input direction vector
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
 	if direction:
-		velocity.x = direction * SPEED
+		# Velocity is a propety of the player2d node
+		# Modify the x and y parts of velocity with direction
+		velocity.x = direction.x * SPEED
+		velocity.y = direction.y * SPEED
 	else:
+		#If we are not moving, the velcoity should move towards 0
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	#Move and slide uses velcoity to move our guy
 	move_and_slide()
