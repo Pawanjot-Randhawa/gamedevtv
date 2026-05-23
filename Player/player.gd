b36acc97661bfb3fd4d537168c9caf8e7209ab6f
Pawanjot Randhawa <pawanjot2004@gmail.com>
2026-05-23 11:52:26 -0400

    many changes

--- Player/player.gd
+++ Player/player.gd
@@ -1,13 +1,24 @@
 extends CharacterBody2D
+class_name  Player
 
 
-const SPEED = 500.0
-#const JUMP_VELOCITY = -400.0
+var SPEED = 500.0
+var ROTATION_SPEED = 2.0
+var POWER = 500
+var CHARGE_TIME = 2
+var HEALTH = 100
 
+var rotation_direction
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("charge"):
+		pass 
 
 func _physics_process(delta: float) -> void:
 	# Get Player input direction vector
 	var direction = Input.get_vector("left", "right", "up", "down")
+	#Get roation Vector
+	rotation_direction = Input.get_axis("RotateLeft", "RotateRight")
 	
 	if direction:
 		# Velocity is a propety of the player2d node
@@ -18,5 +29,7 @@ func _physics_process(delta: float) -> void:
 		#If we are not moving, the velcoity should move towards 0
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+	#Adjust roation based on vector
+	rotation += rotation_direction * ROTATION_SPEED * delta
 	#Move and slide uses velcoity to move our guy
 	move_and_slide()
--- root.gd
+++ root.gd
@@ -1,9 +1,15 @@
 extends Node
 
+@onready var player: Player = $level/Player
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		player.set_process_input(true)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Game.player = player
+	player.set_process_input(false)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
